383dbe9ec84246d3ef26456221b02f82b2daec75
Kye Lewis <kye.lewis@woohah.com.au>
2025-07-03 15:28:08 +1000

    pull out js defs

---
+++ docs/ogscript/rosstalk.md
@@ -0,0 +1,6 @@
+# rosstalk functions
+
+## TCP
+
+### rosstalk.sendMessage - Send a TCP message
+host, port, command, callback
---
+++ e2.grid.js
@@ -0,0 +1,36 @@
+/* Formatter - dashboard does not support trailing commas in function arg lists */
+function E2(ip) {
+  return {
+    recallMultiviewLayout(number) {
+      ogscript.debug("Recalling E2 Multiview Layout...");
+      message =
+        '&lt;System id="0" OPID="-1"&gt;&lt;FrameCollection id="0"&gt;&lt;Frame id="00:13:95:2f:6f:7d"&gt;&lt;MultiViewer id="0"&gt;&lt;LayoutSelect&gt;' +
+        number +
+        "&lt;/LayoutSelect&gt;&lt;/MultiViewer&gt;&lt;/Frame&gt;&lt;/FrameCollection&gt;&lt;/System&gt;\r\n";
+      rosstalk.sendMessage(ip, 9876, message);
+    },
+    getPresets() {
+      ogscript.debug("Getting E2 Presets...");
+      url = "http://" + ip + ":9999/";
+      data =
+        '{"method": "listPresets", "params": { "ScreenDest": -1}, "id": "1234", "jsonrpc": "2.0"}';
+      const response = ogscript.http(
+        url,
+        "POST",
+        "application/json",
+        data,
+        false
+      );
+      const result = JSON.parse(response);
+      return result.result.response;
+    },
+    recallPreset(number) {
+      ogscript.debug("Recalling E2 Preset...");
+      const message =
+        '&lt;System id="0" OPID="-1"&gt;&lt;PresetMgr id="0"&gt;&lt;RecallPresetToPgmTrans&gt;' +
+        number +
+        "&lt;/RecallPresetToPgmTrans&gt;&lt;/PresetMgr&gt;&lt;/System&gt;";
+      rosstalk.sendMessage(ip, 9876, message);
+    },
+  };
+}
