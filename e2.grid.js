f190384bea31c8ac06644be1b20ef2539620e68e
Kye Lewis <kye.lewis@woohah.com.au>
2025-07-04 10:13:56 +1000

    update

--- e2.grid.js
+++ e2.grid.js
@@ -3,9 +3,9 @@ function E2(ip) {
     recallMultiviewLayout(number) {
       ogscript.debug("Recalling E2 Multiview Layout...");
       message =
-        '&lt;System id="0" OPID="-1"&gt;&lt;FrameCollection id="0"&gt;&lt;Frame id="00:13:95:2f:6f:7d"&gt;&lt;MultiViewer id="0"&gt;&lt;LayoutSelect&gt;' +
+        '<System id="0" OPID="-1"><FrameCollection id="0"><Frame id="00:13:95:2f:6f:7d"><MultiViewer id="0"><LayoutSelect>' +
         number +
-        "&lt;/LayoutSelect&gt;&lt;/MultiViewer&gt;&lt;/Frame&gt;&lt;/FrameCollection&gt;&lt;/System&gt;\r\n";
+        "</LayoutSelect></MultiViewer></Frame></FrameCollection></System>\r\n";
       rosstalk.sendMessage(ip, 9876, message);
     },
     getPresets(callback) {
@@ -24,9 +24,9 @@ function E2(ip) {
     recallPreset(number) {
       ogscript.debug("Recalling E2 Preset...");
       const message =
-        '&lt;System id="0" OPID="-1"&gt;&lt;PresetMgr id="0"&gt;&lt;RecallPresetToPgmTrans&gt;' +
+        '<System id="0" OPID="-1"><PresetMgr id="0"><RecallPresetToPgmTrans>' +
         number +
-        "&lt;/RecallPresetToPgmTrans&gt;&lt;/PresetMgr&gt;&lt;/System&gt;";
+        "</RecallPresetToPgmTrans></PresetMgr></System>";
       rosstalk.sendMessage(ip, 9876, message);
     }
   };
