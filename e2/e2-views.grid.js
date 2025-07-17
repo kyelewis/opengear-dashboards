8c3d026b805d4848e34861314cc09f97b4d5a228
Kye Lewis <kye.lewis@woohah.com.au>
2025-07-17 14:06:36 +1000

    e2

--- e2/e2-views.grid.js
+++ e2/e2-views.grid.js
@@ -7,56 +7,74 @@ function generateLabel(name) {
 }
 
 function generateMultiviewerTab() {
+  const ip = params.getValue("e2.ip", 0);
   ogscript.setXML("e2-tab-1", generateLabel("Loading Multivewer Layouts"));
 
-  const layouts = [
-    {
-      Name: "Layout 1",
-      id: 0
-    },
-    {
-      Name: "Layout 2",
-      id: 1
-    },
-    {
-      Name: "Layout 3",
-      id: 2
-    },
-    {
-      Name: "Layout 4",
-      id: 3
+  function _callback(frame_address) {
+    const layouts = [
+      {
+        Name: "Layout 1",
+        id: 0,
+        frame_address: frame_address
+      },
+      {
+        Name: "Layout 2",
+        id: 1,
+        frame_address: frame_address
+      },
+      {
+        Name: "Layout 3",
+        id: 2,
+        frame_address: frame_address
+      },
+      {
+        Name: "Layout 4",
+        id: 3,
+        frame_address: frame_address
+      }
+    ];
+
+    const rows = Math.ceil(layouts.length / 2);
+
+    var content =
+      '<simplegrid id="e2-multiviewer-layouts-grid" rows="' +
+      rows +
+      '" bottom="10" fill="both" left="10" right="10" top="10">';
+
+    for (var i = 0; i < layouts.length; i++) {
+      content +=
+        '<button name="' +
+        layouts[i].Name +
+        '" gpi="mv_' +
+        layouts[i].id +
+        '" width="100" fill="both" buttontype="push">';
+      content += "<task>";
+      content +=
+        'E2("' +
+        ip +
+        '").recallMultiviewLayout("' +
+        layouts[i].frame_address +
+        '", ' +
+        layouts[i].id +
+        ");";
+      content += "</task>";
+      content += "</button>";
     }
-  ];
 
-  const rows = Math.ceil(layouts.length / 2);
+    content += "</simplegrid>";
 
-  var content =
-    '<simplegrid id="e2-multiviewer-layouts-grid" rows="' +
-    rows +
-    '" bottom="10" fill="both" left="10" right="10" top="10">';
-
-  for (var i = 0; i < layouts.length; i++) {
-    content +=
-      '<button name="' +
-      layouts[i].Name +
-      '" width="100" fill="both" buttontype="push">';
-    content += "<task>";
-    content += 'const ip = params.getValue("e2.ip", 0); ';
-    content += 'const frame_address = params.getValue("e2.frame_address", 0);';
-    content +=
-      "E2(ip).recallMultiviewLayout(" + layouts[i].id + ", frame_address);";
-    content += "</task>";
-    content += "</button>";
+    ogscript.setXML("e2-tab-1", content);
   }
 
-  content += "</simplegrid>";
-
-  ogscript.setXML("e2-tab-1", content);
+  // @todo get all frame addresses for layouts, not just the first
+  E2(ip).getFrameAddress(0, _callback);
 }
 
 function generatePresetsTab() {
+  const ip = params.getValue("e2.ip", 0);
   ogscript.setXML("e2-tab-2", generateLabel("Loading Presets"));
-  function callback(presets) {
+
+  function _callback(presets) {
     const rows = Math.ceil(presets.length / 2);
 
     var content =
@@ -65,23 +83,25 @@ function generatePresetsTab() {
       '" fill="both" left="10" right="10" top="10" bottom="10">';
 
     for (var i = 0; i < presets.length; i++) {
+      // @todo use presetSno for gpi
       content +=
-        '<button  name="' +
+        '<button name="' +
         presets[i].Name +
+        " (" +
+        presets[i].presetSno +
+        ')" gpi="preset_' +
+        presets[i].presetSno +
         '" width="100" fill="both" buttontype="push">';
       content += "<task>";
-      content +=
-        'const ip =  params.getValue("e2.ip", 0); E2(ip).recallPreset(' +
-        presets[i].id +
-        ");";
+      content += 'E2("' + ip + '").recallPreset("' + presets[i].id + '");';
       content += "</task>";
       content += "</button>";
     }
 
     content += "</simplegrid>";
+    ogscript.debug(content);
     ogscript.setXML("e2-tab-2", content);
   }
 
-  const ip = params.getValue("e2.ip", 0);
-  E2(ip).getPresets(callback);
+  E2(ip).getPresets(_callback);
 }
--- e2/e2.grid.js
+++ e2/e2.grid.js
@@ -1,27 +1,24 @@
 function E2(ip) {
   return {
-    recallMultiviewLayout(number) {
-      function _callback(frame_address) {
-        ogscript.debug(
-          "Recalling E2 Multiview Layout " +
-            number +
-            " to " +
-            ip +
-            ":9876" +
-            " frame: " +
-            frame_address
-        );
-        message =
-          '<System id="0" OPID="-1"><FrameCollection id="0"><Frame id="' +
-          frame_address +
-          '"><MultiViewer id="0"><LayoutSelect>' +
-          number +
-          "</LayoutSelect></MultiViewer></Frame></FrameCollection></System>\r\n";
-        ogscript.debug(message);
-        rosstalk.sendMessage(ip, 9876, message);
-      }
-
-      this.getFrameAddress(0, _callback);
+    recallMultiviewLayout(frame_address, index) {
+      // multiviewer grid
+      ogscript.debug(
+        "Recalling E2 Multiview Layout " +
+          index +
+          " to " +
+          ip +
+          ":9876" +
+          " for frame " +
+          frame_address
+      );
+      message =
+        '<System id="0" OPID="-1"><FrameCollection id="0"><Frame id="' +
+        frame_address +
+        '"><MultiViewer id="0"><LayoutSelect>' +
+        index +
+        "</LayoutSelect></MultiViewer></Frame></FrameCollection></System>\r\n";
+      ogscript.debug(message);
+      rosstalk.sendMessage(ip, 9876, message);
     },
     getFrameAddress(index, callback) {
       function _callback(response) {
@@ -54,23 +51,18 @@ function E2(ip) {
         var presetsOut = [];
 
         for (i = 0; i < presets.getLength(); i++) {
-          ogscript.debug("Preset");
           var preset = presets.item(i);
           var Name = ogscript
             .runXPath("./Name", preset)
             .item(0)
             .getTextContent();
-          var id = ogscript.runXPath("./@id", preset).item(0);
-          presetsOut.push({ Name: Name, id: id });
+          var presetSno = ogscript
+            .runXPath("./presetSno", preset)
+            .item(0)
+            .getTextContent();
+          var id = ogscript.runXPath("./@id", preset).item(0).getTextContent();
+          presetsOut.push({ presetSno: presetSno, Name: Name, id: id });
         }
-        ogscript.debug(
-          "Presets loaded:" +
-            presetsOut
-              .map(function (p) {
-                return p.Name;
-              })
-              .join(",")
-        );
 
         callback(presetsOut);
       }
@@ -79,21 +71,24 @@ function E2(ip) {
         '<System id="0"><XMLType>3</XMLType><Query>3</Query><Recursive>1</Recursive></System>';
       this.sendXml(xml, _callback);
     },
-    recallPreset(number) {
-      ogscript.debug("Recalling E2 Preset " + number + " to " + ip + ":9876");
-      const message =
+    recallPreset(id) {
+      ogscript.debug("Recalling E2 Preset ID #" + id + " to " + ip + ":9876");
+      function _callback(_response) {}
+      const xml =
         '<System id="0" OPID="-1"><PresetMgr id="0"><RecallPresetToPgmTrans>' +
-        number +
+        id +
         "</RecallPresetToPgmTrans></PresetMgr></System>";
-      rosstalk.sendMessage(ip, 9876, message);
+      this.sendXml(xml, _callback);
     },
 
     sendXml(xml, callback) {
       function _callback(success, _, response, _) {
         if (success) {
+          ogscript.debug("Received response: " + response + ">");
           callback(response + ">");
         }
       }
+      ogscript.debug("Sending: " + xml);
       rosstalk.sendMessageWithResponse(ip, 9876, xml, "</System>", _callback);
     }
   };
